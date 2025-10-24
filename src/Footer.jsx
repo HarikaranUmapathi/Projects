46e636956fc74d3e5778c79282898a6eba7486f6
HarikaranUmapathi <168574723+HarikaranUmapathi@users.noreply.github.com>
2025-10-24 12:33:40 +0530

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className="mt-[10rem]  flex justify-center">
-      <h1>Copyright © Neat Starter 2020. Made with ❤ by HariKaran U • View on Github</h1>
-    </div>
-  )
-}
--- src/Home/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-function Homepage(){
-    return(
-        <div>
-            <h1>Harikaran U</h1>
-        </div>
-    )
-}
-
-export default Homepage;
